509a1f95731bdacf8972c1ea3c8bca9a3297a4e1
Dan Bikel <dan@bikel.net>
2015-05-07 15:36:25 -0700

    Update docs to include documentation on Interpreter::GetMany method.

--- search/all_6.js
+++ search/all_6.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['get',['Get',['../classinfact_1_1_environment_impl.html#a4c7040cdaade112f6cef88cff8c4ee6c',1,'infact::EnvironmentImpl::Get()'],['../classinfact_1_1_var_map_impl.html#a7995e8a182a980899b4a0c90eee0d110',1,'infact::VarMapImpl::Get()'],['../classinfact_1_1_interpreter.html#ae9a966d8b7d4fc9c112070d133407c41',1,'infact::Interpreter::Get()']]],
+  ['getmany',['GetMany',['../classinfact_1_1_interpreter.html#ad2f9b4f584739468e7202c30a5f8e76c',1,'infact::Interpreter::GetMany()'],['../classinfact_1_1_interpreter.html#ace1bded7b258854a0dca1476dbe3177c',1,'infact::Interpreter::GetMany(const string &amp;varname, T *var, Args &amp;&amp;...rest)']]],
   ['getnumberofpets',['GetNumberOfPets',['../classinfact_1_1_pet_owner.html#ad69dd29b3deed52c57f02a384761979e',1,'infact::PetOwner::GetNumberOfPets()'],['../classinfact_1_1_human_pet_owner.html#ac50757937a9715f9151b4a46d3db7373',1,'infact::HumanPetOwner::GetNumberOfPets()']]],
   ['getpet',['GetPet',['../classinfact_1_1_pet_owner.html#ad02375b2fb31e6463ddec16d02ac39cf',1,'infact::PetOwner::GetPet()'],['../classinfact_1_1_human_pet_owner.html#a9c680a005a4d0746dd3cbaca95b75068',1,'infact::HumanPetOwner::GetPet()']]],
   ['gettype',['GetType',['../classinfact_1_1_environment_impl.html#a7e0f9fb13e56dbcff3bc758ee891c576',1,'infact::EnvironmentImpl::GetType()'],['../classinfact_1_1_environment.html#a2ccb9f963c093da31f504bd4021682a5',1,'infact::Environment::GetType()']]],
